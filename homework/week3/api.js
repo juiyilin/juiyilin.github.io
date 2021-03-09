16bf74e477d01344991fd12acac53addf0d3cd95
j2 <join830925@gmail.com>
2021-03-09 16:21:15 +0800

    add ; at api.js

--- homework/week3/api.js
+++ homework/week3/api.js
@@ -1,4 +1,4 @@
-let url = 'https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json'
+let url = 'https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json';
 let count = 1;
 
 
@@ -25,7 +25,7 @@ function showdata(data, cur, ct) {
         let text = document.createTextNode(spot.stitle);
 
         cardText.appendChild(text);
-        cardClass.appendChild(imgClass).appendChild(cardText)
+        cardClass.appendChild(imgClass).appendChild(cardText);
         content.appendChild(cardClass);
 
     };
@@ -37,15 +37,15 @@ window.addEventListener('load', function () {
         return response.json();
     }).then(function (data) {
         showdata(data, 0, count);
-    })
-})
+    });
+});
 
 let btn = document.getElementById('btn');
 btn.addEventListener('click', function () {
     fetch(url).then(function (response) {
         return response.json();
     }).then(function (data) {
-        let curCount = document.getElementsByClassName('card').length
+        let curCount = document.getElementsByClassName('card').length;
         console.log('.card:', curCount);
         count += 1;
         showdata(data, curCount, count);
